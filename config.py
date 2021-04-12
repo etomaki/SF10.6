0f75fa295ae4d3bd67298de19988a54c5d0b0f4f
etomaki <70105692+etomaki@users.noreply.github.com>
2021-04-13 00:13:43 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '1511958499:AAHjheh3jZpCpSkYmxGln-y9jyB6-x0TM74'
+CURRENCY_API = 'c699c1bde1dd304c11cd646a54220fb3'
+#AVALIABLE CURRENCY
+val = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
+symbols = {
+    'EUR' : '€',
+    'RUB' : '₽',
+    'USD' : '$'
+}
